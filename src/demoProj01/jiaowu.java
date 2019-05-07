5c2377840b3bd3ec1d6fd64eaf4866cafce425fd
Administrator <Administrator@WIN-4AB7V98BU57>
2019-05-07 20:17:05 +0800

    Signed-off-by: Administrator <Administrator@WIN-4AB7V98BU57>

--- src/demoProj01/jiaowu.java
+++ src/demoProj01/jiaowu.java
@@ -1,8 +1,8 @@
-package demoProj;
+package demoProj01;
 
 public class jiaowu {
 
 	public static void main(String[] args) {
-		System.out.println("good");
+		System.out.println("good branch!");
 	}
 }
